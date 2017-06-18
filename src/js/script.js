c709a0040b573e96f01aa4336bdd5aae92e98e8b
FS <EnotPolackun@mail.ru>
2017-06-18 21:08:39 +0700

    Add list folders in array (Split input data)

--- src/js/script.js
+++ src/js/script.js
@@ -11,22 +11,28 @@ $(function(){
 	var gui = require('nw.gui');
 });
 
+
+
+function data_of_form ()
+{
+	var arr_data = {};
+	arr_data["list_input_folder"] = $("#list_input_folder").val();
+	arr_data["output_folder"] = $("#output_folder").val();
+	arr_data["type_mover"] = ($("#copy").prop("checked")) ? "copy" : "move";
+	arr_data["remove_copies"] = $("#ubrat_copy").prop("checked");
+	arr_data["type_remove_copies"] = ($("#global").prop("checked")) ? "global" : "local";
+
+	return arr_data;
+}
 $("#group_proceccing_1").click(function(){
-	alert(
-		$("#list_input_folder").val() + "\n"
-
-		+ $("#output_folder").val() + " : " + $("#output_folder").prop("checked") + "\n"
-		+ $("#copy").val() + " : " + $("#copy").prop("checked") + "\n"
-		+ $("#move").val() + " : " + $("#move").prop("checked") + "\n"
-		+ $("#ubrat_copy").val() + " : " + $("#ubrat_copy").prop("checked") + "\n"
-		+ $("#global").val() + " : " + $("#global").prop("checked") + "\n"
-		+ $("#local").val() + " : " + $("#local").prop("checked") + "\n"
-		
-		
-		);
-
-	var folders = $("#list_input_folder").val().split('\n');
+	var arr_data = {};
+	var arr_data = data_of_form();
+	var str_folders = arr_data["list_input_folder"];
+	var folders = str_folders.split('\n');
+	var text = "";
 	jQuery.each(folders, function(i, val)
 	{
-	}); 
+		text += val + "\n";
+	});
+	alert(text);
 });
