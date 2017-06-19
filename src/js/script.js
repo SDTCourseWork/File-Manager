c3e5de9558d5b1f886c80845f38179ed6b996958
FS <EnotPolackun@mail.ru>
2017-06-19 08:31:31 +0700

    Add hash of file

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,92 @@
-// Mixing jQuery and Node.js code in the same file? Yes please!
 
-$(function()
+$("#group_proceccing_1").click(function()
+{
+	var arr_data = data_of_form();
+	var folders = arr_data["list_input_folder"].split('\n');
+
+	jQuery.each(folders, function(i, str_path)
+	{
+		var list_files = getFiles(str_path);
+		jQuery.each(list_files, function(i, file)
+		{
+			processing_of_files(file);
+		});
+	});
+	//очищение для локальной обработки
+});
+
+
+
+
+
+
+
+
+
+
+function isCopy (file)
+{
+	var hash = hash_of_file(file);
+	if (typeof LIST_HASH[hash] == "undefined")
+	{
+		LIST_HASH[hash] = 1;
+		return false;
+	}
+	else
+		return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+var LIST_HASH = {};
+function processing_of_files (file)
 {
 
-});
+	$('.stats').append("<br>processing: " + file + " " + isCopy(file));
+}
+
+
+
+
+
+
+
+
+
+
+
+var fs = require('fs');
+var path = require('path');
+var getFiles = function (dir, files_){
+	files_ = files_ || [];
+	var files = fs.readdirSync(dir);
+	for (var i in files){
+		var name = dir + '\\' + files[i];
+		if (fs.statSync(name).isDirectory()){
+			getFiles(name, files_);
+		} else {
+			files_.push(name);
+		}
+	}
+	return files_;
+};
+
+
+
+
+
+
+
+
+
 
 
 
@@ -19,49 +102,34 @@ function data_of_form ()
 	return arr_data;
 }
 
- 
-
- 
 
 
+
+
+
+
+
+
+
+
+
+const crypto = require('crypto');
 var fs = require('fs');
-var path = require('path');
-var getFiles = function (dir, files_){
-	files_ = files_ || [];
-	var files = fs.readdirSync(dir);
-	for (var i in files){
-		var name = dir + '/' + files[i];
-		if (fs.statSync(name).isDirectory()){
-			getFiles(name, files_);
-		} else {
-			files_.push(name);
-		}
-	}
-	return files_;
-};
-$("#group_proceccing_1").click(function()
+function hash_of_file(filename)
 {
-	var arr_data = data_of_form();
-	var folders = arr_data["list_input_folder"].split('\n');
+	const hash = crypto.createHash('md5');
 
-	jQuery.each(folders, function(i, str_path)
-	{
-		var list_files = getFiles(str_path);
-		jQuery.each(list_files, function(i, file)
-		{
-			processing_of_files(file);
-		});
-	});
-});
+	// const input = fs.createReadStream(filename);
+	// input.on('readable', () => {
+	// 	var data = "alica"; //input.read();
+	// 	if (data)
+	// 		hash.update(data);
+	// 	else {
+	// 		text_hash += hash.digest('hex');
+	// 	}
+	// });
+	var text = fs.readFileSync(filename, 'utf8');
+	hash.update(text);
 
-
-
-
-
-
-
-
-function processing_of_files (file)
-{
-	$('.stats').append("<br>processing: " + file);
+	return hash.digest('hex');
 }
